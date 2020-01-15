8d3bad25a19b81086a55983ea36421247fbf9388
strydfelt <strydfelt@gmail.com>
2020-01-15 13:44:48 +0800

    works on lollipop, not on pie

--- app/src/main/java/sg/govtech/fellow/boot/StartOnBootReceiver.java
+++ app/src/main/java/sg/govtech/fellow/boot/StartOnBootReceiver.java
@@ -5,14 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 
 import sg.govtech.fellow.location.LocationUpdatesService;
+import sg.govtech.fellow.log.SDLog;
 
 public class StartOnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        SDLog.setAppName("Fellow");
+
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            SDLog.d("boot completed received");
             Intent serviceIntent = new Intent(context, LocationUpdatesService.class);
             serviceIntent.putExtra(LocationUpdatesService.COMMAND_KEY, LocationUpdatesService.ACTION_START);
             context.startService(serviceIntent);
+            SDLog.d("Attempting to start service");
         }
     }
 }
